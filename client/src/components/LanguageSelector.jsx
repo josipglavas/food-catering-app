f306ca66fd4ba6db0efe33ad9341be356bee74e1
josipglavas <glavas.josip@icloud.com>
2024-08-02 19:16:31 +0200

    Fixed flags

--- client/src/components/LanguageSelector.jsx
+++ client/src/components/LanguageSelector.jsx
@@ -1,23 +1,40 @@
 import { useTranslation } from "react-i18next";
-import Flag from "react-flagpack";
+import ReactCountryFlag from "react-country-flag";
 
 const LanguageSelector = () => {
   const { t, i18n } = useTranslation();
-
+  const currentLanguage = i18n.language;
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang);
   };
 
   return (
-    <div className="flex gap-1">
-      <button onClick={() => changeLanguage("en")} className="flex gap-1">
-        <Flag code="USA" gradient="real-linear" size="l" hasDropShadow />
-        <p>EN</p>
-      </button>
-      <button onClick={() => changeLanguage("hr")} className="flex gap-1">
-        <Flag code="HRV" size="l" hasDropShadow />
-        <p>HR</p>
-      </button>
+    <div className="flex gap-1 content-center justify-center">
+      {currentLanguage === "en" ? (
+        <button onClick={() => changeLanguage("hr")} className="flex gap-1">
+          <ReactCountryFlag
+            countryCode="HR"
+            svg
+            style={{
+              fontSize: "1.5em",
+              lineHeight: "1.5em",
+            }}
+          />
+          <p>HR</p>
+        </button>
+      ) : (
+        <button onClick={() => changeLanguage("en")} className="flex gap-1">
+          <ReactCountryFlag
+            countryCode="US"
+            svg
+            style={{
+              fontSize: "1.5em",
+              lineHeight: "1.5em",
+            }}
+          />
+          <p>EN</p>
+        </button>
+      )}
     </div>
   );
 };
