8fce01f0bd0fc9b0a2b1d5ae342038cd10c801a2
josipglavas <glavas.josip@icloud.com>
2024-08-06 23:19:25 +0200

    updated footer translation

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,8 +17,11 @@ import {
 } from "react-icons/bs";
 
 import LogoFooter from "../assets/logo-footer.png";
+import { useTranslation } from "react-i18next";
 
 export function FooterComponent() {
+  const { t } = useTranslation();
+
   return (
     <Footer container className="pt-6 bg-slate-950">
       <div className="w-full px-24">
@@ -28,7 +31,7 @@ export function FooterComponent() {
           </div>
           <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
-              <FooterTitle title="about" />
+              <FooterTitle title={t("Footer_About")} />
               <FooterLinkGroup col>
                 <FooterLink href="#">
                   <p className="text-slate-300">Flowbite</p>
@@ -39,7 +42,7 @@ export function FooterComponent() {
               </FooterLinkGroup>
             </div>
             <div>
-              <FooterTitle title="Follow us" />
+              <FooterTitle title={t("Footer_FollowUs")} />
               <FooterLinkGroup col>
                 <FooterLink href="#">
                   <p className="text-slate-300">Github</p>
@@ -50,7 +53,7 @@ export function FooterComponent() {
               </FooterLinkGroup>
             </div>
             <div>
-              <FooterTitle title="Legal" />
+              <FooterTitle title={t("Footer_Legal")} />
               <FooterLinkGroup col>
                 <FooterLink href="#">
                   <p className="text-slate-300">Privacy Policy</p>
@@ -64,7 +67,7 @@ export function FooterComponent() {
         </div>
         <FooterDivider />
         <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <FooterCopyright href="#" by="OuiChef™" year={2024} />
+          <FooterCopyright href="/" by="OuiChef™" year={2024} />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
             <FooterIcon href="#" icon={BsFacebook} />
             <FooterIcon href="#" icon={BsInstagram} />
