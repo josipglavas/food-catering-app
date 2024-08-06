a1dea4687888035c78ce2dbf1ad8efced2b20020
josipglavas <glavas.josip@icloud.com>
2024-08-06 22:51:01 +0200

    Updated error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,16 +1,29 @@
 import { useRouteError } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.error(error);
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="h-screen bg-neutral-200">
+      <Navbar />
+      <div id="error-page" className="px-20 py-8">
+        <h1 className="text-7xl pb-10">Oops!</h1>
+        <p className="text-2xl">{t("Error_Paragraph")}</p>
+        <Button
+          Class={"uppercase mt-10"}
+          Text={t("Error_Btn_Return")}
+          onClick={() => navigate("/contact")}
+        />
+        {/* <p>
+          <i>{error.statusText || error.message}</i>
+        </p> */}
+      </div>
     </div>
   );
 };
